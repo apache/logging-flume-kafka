dfab6d5c4e2573ad85f552d440485a2f88cabba8
Ralph Goers <rgoers@apache.org>
2026-06-27 16:43:39 -0700

    Upgrade to Kafka 4

--- flume-kafka-source/src/main/java/org/apache/flume/source/kafka/KafkaSourceConstants.java
+++ flume-kafka-source/src/main/java/org/apache/flume/source/kafka/KafkaSourceConstants.java
@@ -2,7 +2,7 @@
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
+ * The ASF licenses this file to you under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
@@ -20,38 +20,33 @@ import org.apache.kafka.clients.CommonClientConfigs;
 
 public class KafkaSourceConstants {
 
-  public static final String KAFKA_PREFIX = "kafka.";
-  public static final String KAFKA_CONSUMER_PREFIX = KAFKA_PREFIX + "consumer.";
-  public static final String DEFAULT_KEY_DESERIALIZER =
-      "org.apache.kafka.common.serialization.StringDeserializer";
-  public static final String DEFAULT_VALUE_DESERIALIZER =
-      "org.apache.kafka.common.serialization.ByteArrayDeserializer";
-  public static final String BOOTSTRAP_SERVERS =
-      KAFKA_PREFIX + CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
-  public static final String TOPICS = KAFKA_PREFIX + "topics";
-  public static final String TOPICS_REGEX = TOPICS + "." + "regex";
-  public static final String DEFAULT_AUTO_COMMIT =  "false";
-  public static final String BATCH_SIZE = "batchSize";
-  public static final String BATCH_DURATION_MS = "batchDurationMillis";
-  public static final int DEFAULT_BATCH_SIZE = 1000;
-  public static final int DEFAULT_BATCH_DURATION = 1000;
-  public static final String DEFAULT_GROUP_ID = "flume";
-  public static final String KAFKA_HEADER = "header.";
+    public static final String KAFKA_PREFIX = "kafka.";
+    public static final String KAFKA_CONSUMER_PREFIX = KAFKA_PREFIX + "consumer.";
+    public static final String DEFAULT_KEY_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer";
+    public static final String DEFAULT_VALUE_DESERIALIZER =
+            "org.apache.kafka.common.serialization.ByteArrayDeserializer";
+    public static final String BOOTSTRAP_SERVERS = KAFKA_PREFIX + CommonClientConfigs.BOOTSTRAP_SERVERS_CONFIG;
+    public static final String TOPICS = KAFKA_PREFIX + "topics";
+    public static final String TOPICS_REGEX = TOPICS + "." + "regex";
+    public static final String DEFAULT_AUTO_COMMIT = "false";
+    public static final String BATCH_SIZE = "batchSize";
+    public static final String BATCH_DURATION_MS = "batchDurationMillis";
+    public static final int DEFAULT_BATCH_SIZE = 1000;
+    public static final int DEFAULT_BATCH_DURATION = 1000;
+    public static final String DEFAULT_GROUP_ID = "flume";
+    public static final String KAFKA_HEADER = "header.";
 
-  public static final String AVRO_EVENT = "useFlumeEventFormat";
-  public static final boolean DEFAULT_AVRO_EVENT = false;
+    public static final String AVRO_EVENT = "useFlumeEventFormat";
+    public static final boolean DEFAULT_AVRO_EVENT = false;
 
-  // flume event headers
-  public static final String DEFAULT_TOPIC_HEADER = "topic";
-  public static final String KEY_HEADER = "key";
-  public static final String TIMESTAMP_HEADER = "timestamp";
-  public static final String PARTITION_HEADER = "partition";
-  public static final String OFFSET_HEADER = "offset";
+    // flume event headers
+    public static final String DEFAULT_TOPIC_HEADER = "topic";
+    public static final String KEY_HEADER = "key";
+    public static final String TIMESTAMP_HEADER = "timestamp";
+    public static final String PARTITION_HEADER = "partition";
+    public static final String OFFSET_HEADER = "offset";
 
-  public static final String SET_TOPIC_HEADER = "setTopicHeader";
-  public static final boolean DEFAULT_SET_TOPIC_HEADER = true;
-  public static final String TOPIC_HEADER = "topicHeader";
-
-
-
+    public static final String SET_TOPIC_HEADER = "setTopicHeader";
+    public static final boolean DEFAULT_SET_TOPIC_HEADER = true;
+    public static final String TOPIC_HEADER = "topicHeader";
 }
--- flume-kafka-source/src/test/java/org/apache/flume/source/kafka/KafkaSourceEmbeddedKafka.java
+++ flume-kafka-source/src/test/java/org/apache/flume/source/kafka/KafkaSourceEmbeddedKafka.java
@@ -2,7 +2,7 @@
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
  * this work for additional information regarding copyright ownership.
- * The ASF licenses this file to You under the Apache License, Version 2.0
+ * The ASF licenses this file to you under the Apache License, Version 2.0
  * (the "License"); you may not use this file except in compliance with
  * the License.  You may obtain a copy of the License at
  *
@@ -16,10 +16,23 @@
  */
 package org.apache.flume.source.kafka;
 
-import kafka.server.KafkaConfig;
-import kafka.server.KafkaServer;
-import kafka.utils.TestUtils;
-import org.apache.commons.io.FileUtils;
+import static org.apache.kafka.common.config.SslConfigs.SSL_KEYSTORE_LOCATION_CONFIG;
+import static org.apache.kafka.common.config.SslConfigs.SSL_KEYSTORE_PASSWORD_CONFIG;
+import static org.apache.kafka.common.config.SslConfigs.SSL_KEY_PASSWORD_CONFIG;
+import static org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_LOCATION_CONFIG;
+import static org.apache.kafka.common.config.SslConfigs.SSL_TRUSTSTORE_PASSWORD_CONFIG;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import kafka.server.BrokerServer;
 import org.apache.kafka.clients.admin.AdminClient;
 import org.apache.kafka.clients.admin.CreateTopicsResult;
 import org.apache.kafka.clients.admin.NewTopic;
@@ -27,183 +40,193 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.header.Headers;
+import org.apache.kafka.common.network.ListenerName;
+import org.apache.kafka.common.security.auth.SecurityProtocol;
 import org.apache.kafka.common.serialization.ByteArraySerializer;
 import org.apache.kafka.common.serialization.StringSerializer;
-import org.apache.kafka.common.utils.Time;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.net.ServerSocket;
-import java.util.Collections;
-import java.util.List;
-import java.util.Properties;
-import java.util.UUID;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.TimeUnit;
-
-import static org.apache.kafka.common.config.SslConfigs.*;
+import org.apache.kafka.common.test.KafkaClusterTestKit;
+import org.apache.kafka.common.test.TestKitNodes;
 
 public class KafkaSourceEmbeddedKafka {
 
-  public static String HOST = InetAddress.getLoopbackAddress().getCanonicalHostName();
+    private static final String CONTROLLER_LISTENER_NAME = "CONTROLLER";
+    private static final String PLAINTEXT_LISTENER_NAME = "PLAINTEXT";
+    private static final String SSL_LISTENER_NAME = "SSL";
 
-  KafkaServer kafkaServer;
-  KafkaSourceEmbeddedZookeeper zookeeper;
-  private AdminClient adminClient;
+    public static final String HOST = InetAddress.getLoopbackAddress().getCanonicalHostName();
 
-  private static int findFreePort() {
-    try (ServerSocket socket = new ServerSocket(0)) {
-      return socket.getLocalPort();
-    } catch (IOException e) {
-      throw new AssertionError("Can not find free port.", e);
+    private final KafkaClusterTestKit cluster;
+    private final String bootstrapServers;
+    private final String bootstrapSslServers;
+    private final String bootstrapSslIpPortServers;
+    private AdminClient adminClient;
+    private KafkaProducer<String, byte[]> producer;
+
+    public KafkaSourceEmbeddedKafka(Properties properties) {
+        try {
+            File baseDirectory = new File("target/kafka-source-cluster-" + UUID.randomUUID()).getAbsoluteFile();
+            TestKitNodes nodes = new TestKitNodes.Builder()
+                    .setCombined(true)
+                    .setNumControllerNodes(1)
+                    .setNumBrokerNodes(1)
+                    .setBaseDirectory(baseDirectory.toPath())
+                    .setBrokerListenerName(ListenerName.normalised(PLAINTEXT_LISTENER_NAME))
+                    .setBrokerSecurityProtocol(SecurityProtocol.PLAINTEXT)
+                    .setControllerListenerName(ListenerName.normalised(CONTROLLER_LISTENER_NAME))
+                    .setControllerSecurityProtocol(SecurityProtocol.PLAINTEXT)
+                    .build();
+
+            KafkaClusterTestKit.Builder builder = new KafkaClusterTestKit.Builder(nodes)
+                    .setConfigProp(
+                            "listeners",
+                            PLAINTEXT_LISTENER_NAME + "://localhost:0,"
+                                    + SSL_LISTENER_NAME + "://localhost:0,"
+                                    + CONTROLLER_LISTENER_NAME + "://localhost:0")
+                    .setConfigProp(
+                            "listener.security.protocol.map",
+                            PLAINTEXT_LISTENER_NAME + ":PLAINTEXT,"
+                                    + SSL_LISTENER_NAME + ":SSL,"
+                                    + CONTROLLER_LISTENER_NAME + ":PLAINTEXT")
+                    .setConfigProp("inter.broker.listener.name", PLAINTEXT_LISTENER_NAME)
+                    .setConfigProp("controller.listener.names", CONTROLLER_LISTENER_NAME)
+                    .setConfigProp("offsets.topic.replication.factor", "1")
+                    .setConfigProp("transaction.state.log.replication.factor", "1")
+                    .setConfigProp("transaction.state.log.min.isr", "1")
+                    .setConfigProp("transaction.state.log.num.partitions", "1")
+                    .setConfigProp("auto.create.topics.enable", "false")
+                    .setConfigProp(SSL_TRUSTSTORE_LOCATION_CONFIG, "src/test/resources/truststorefile.jks")
+                    .setConfigProp(SSL_TRUSTSTORE_PASSWORD_CONFIG, "password")
+                    .setConfigProp(SSL_KEYSTORE_LOCATION_CONFIG, "src/test/resources/keystorefile.jks")
+                    .setConfigProp(SSL_KEYSTORE_PASSWORD_CONFIG, "password")
+                    .setConfigProp(SSL_KEY_PASSWORD_CONFIG, "password");
+            if (properties != null) {
+                for (Map.Entry<Object, Object> entry : properties.entrySet()) {
+                    builder.setConfigProp(String.valueOf(entry.getKey()), entry.getValue());
+                }
+            }
+            cluster = builder.build();
+            cluster.format();
+            cluster.startup();
+            cluster.waitForReadyBrokers();
+            bootstrapServers = cluster.bootstrapServers();
+            int sslPort = getBoundPort(SSL_LISTENER_NAME);
+            bootstrapSslServers = HOST + ":" + sslPort;
+            bootstrapSslIpPortServers = "127.0.0.1:" + sslPort;
+            initProducer();
+        } catch (Exception e) {
+            throw new RuntimeException("Unable to start embedded Kafka cluster", e);
+        }
     }
-  }
 
-  private int zkPort = findFreePort(); // none-standard
-  private int serverPort = findFreePort();
-  private int serverSslPort = findFreePort();
-
-  KafkaProducer<String, byte[]> producer;
-  File dir;
-
-  public KafkaSourceEmbeddedKafka(Properties properties) {
-    zookeeper = new KafkaSourceEmbeddedZookeeper(zkPort);
-    dir = new File(System.getProperty("java.io.tmpdir"), "kafka_log-" + UUID.randomUUID());
-    try {
-      FileUtils.deleteDirectory(dir);
-    } catch (IOException e) {
-      e.printStackTrace();
+    public void stop() throws IOException {
+        if (producer != null) {
+            producer.close();
+        }
+        if (adminClient != null) {
+            adminClient.close();
+            adminClient = null;
+        }
+        try {
+            cluster.close();
+        } catch (Exception e) {
+            throw new IOException("Unable to stop embedded Kafka cluster", e);
+        }
     }
-    Properties props = new Properties();
-    props.put("zookeeper.connect",zookeeper.getConnectString());
-    props.put("broker.id","1");
-    props.put("host.name", "localhost");
-    //  to enable ssl feature,
-    //  we need to use listeners instead of using port property
-    //  props.put("port", String.valueOf(serverPort));
-    props.put("listeners",
-            String.format("PLAINTEXT://%s:%d,SSL://%s:%d",
-                    HOST,
-                    serverPort,
-                    HOST,
-                    serverSslPort
-            )
-    );
-    props.put("log.dir", dir.getAbsolutePath());
-    props.put("offsets.topic.replication.factor", "1");
-    props.put("auto.create.topics.enable", "false");
-    //  ssl configuration
-    props.put(SSL_TRUSTSTORE_LOCATION_CONFIG, "src/test/resources/truststorefile.jks");
-    props.put(SSL_TRUSTSTORE_PASSWORD_CONFIG, "password");
-    props.put(SSL_KEYSTORE_LOCATION_CONFIG, "src/test/resources/keystorefile.jks");
-    props.put(SSL_KEYSTORE_PASSWORD_CONFIG, "password");
-    if (properties != null) {
-      props.putAll(properties);
+
+    public String getZkConnectString() {
+        return null;
     }
-    KafkaConfig config = new KafkaConfig(props);
-    kafkaServer = TestUtils.createServer(config, Time.SYSTEM);
-    kafkaServer.startup();
-    initProducer();
-  }
 
-  public void stop() throws IOException {
-    producer.close();
-    kafkaServer.shutdown();
-    zookeeper.stopZookeeper();
-  }
-
-  public String getZkConnectString() {
-    return zookeeper.getConnectString();
-  }
-
-  public String getBootstrapServers() {
-    return HOST + ":" + serverPort;
-  }
-
-  public String getBootstrapSslServers() {
-    return String.format("%s:%s", HOST, serverSslPort);
-  }
-
-  public String getBootstrapSslIpPortServers() {
-    return String.format("%s:%s", "127.0.0.1", serverSslPort);
-  }
-
-  private void initProducer() {
-    Properties props = new Properties();
-    props.put("bootstrap.servers", HOST + ":" + serverPort);
-    props.put("acks", "1");
-    producer = new KafkaProducer<String,byte[]>(props,
-            new StringSerializer(), new ByteArraySerializer());
-  }
-
-  public void produce(String topic, String k, String v) {
-    produce(topic, k, v.getBytes());
-  }
-
-  public void produce(String topic, String k, byte[] v) {
-    ProducerRecord<String, byte[]> rec = new ProducerRecord<>(topic, k, v);
-    try {
-      producer.send(rec).get();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } catch (ExecutionException e) {
-      e.printStackTrace();
+    public String getBootstrapServers() {
+        return bootstrapServers;
     }
-  }
 
-  public void produce(String topic, int partition, String k, String v) {
-    produce(topic,partition,k,v.getBytes());
-  }
-
-  public void produce(String topic, int partition, String k, byte[] v) {
-    this.produce(topic, partition, null, k, v, null);
-  }
-
-  public void produce(String topic, int partition, Long timestamp, String k, byte[] v,
-      Headers headers) {
-    ProducerRecord<String, byte[]> rec = new ProducerRecord<>(topic, partition, timestamp, k, v,
-        headers);
-    try {
-      producer.send(rec).get();
-    } catch (InterruptedException e) {
-      e.printStackTrace();
-    } catch (ExecutionException e) {
-      e.printStackTrace();
+    public String getBootstrapSslServers() {
+        return bootstrapSslServers;
     }
-  }
 
-  public void createTopic(String topicName, int numPartitions) {
-    AdminClient adminClient = getAdminClient();
-    NewTopic newTopic = new NewTopic(topicName, numPartitions, (short) 1);
-    CreateTopicsResult result = adminClient.createTopics(Collections.singletonList(newTopic));
-    Throwable throwable = null;
-    for (int i = 0; i < 10; ++i) {
-      try {
-        result.all().get(1, TimeUnit.SECONDS);
-        throwable = null;
-        break;
-      } catch (Exception e) {
-        throwable = e;
-      }
+    public String getBootstrapSslIpPortServers() {
+        return bootstrapSslIpPortServers;
     }
-    if (throwable != null) {
-      throw new RuntimeException("Error getting topic info", throwable);
+
+    private void initProducer() {
+        Properties props = new Properties();
+        props.put("bootstrap.servers", bootstrapServers);
+        props.put("acks", "1");
+        producer = new KafkaProducer<>(props, new StringSerializer(), new ByteArraySerializer());
     }
-  }
 
-  private AdminClient getAdminClient() {
-    if (adminClient == null) {
-      final Properties props = new Properties();
-      props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, HOST + ":" + serverPort);
-      props.put(ConsumerConfig.GROUP_ID_CONFIG, "group_1");
-      adminClient = AdminClient.create(props);
+    public void produce(String topic, String k, String v) {
+        produce(topic, k, v.getBytes());
     }
-    return adminClient;
-  }
 
-  public void deleteTopics(List<String> topic) {
-    getAdminClient().deleteTopics(topic);
-  }
+    public void produce(String topic, String k, byte[] v) {
+        ProducerRecord<String, byte[]> rec = new ProducerRecord<>(topic, k, v);
+        try {
+            producer.send(rec).get();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Interrupted while producing message", e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException("Error producing message", e);
+        }
+    }
 
+    public void produce(String topic, int partition, String k, String v) {
+        produce(topic, partition, k, v.getBytes());
+    }
+
+    public void produce(String topic, int partition, String k, byte[] v) {
+        this.produce(topic, partition, null, k, v, null);
+    }
+
+    public void produce(String topic, int partition, Long timestamp, String k, byte[] v, Headers headers) {
+        ProducerRecord<String, byte[]> rec = new ProducerRecord<>(topic, partition, timestamp, k, v, headers);
+        try {
+            producer.send(rec).get();
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+            throw new RuntimeException("Interrupted while producing message", e);
+        } catch (ExecutionException e) {
+            throw new RuntimeException("Error producing message", e);
+        }
+    }
+
+    public void createTopic(String topicName, int numPartitions) {
+        AdminClient adminClient = getAdminClient();
+        NewTopic newTopic = new NewTopic(topicName, numPartitions, (short) 1);
+        CreateTopicsResult result = adminClient.createTopics(Collections.singletonList(newTopic));
+        Throwable throwable = null;
+        for (int i = 0; i < 10; ++i) {
+            try {
+                result.all().get(1, TimeUnit.SECONDS);
+                throwable = null;
+                break;
+            } catch (Exception e) {
+                throwable = e;
+            }
+        }
+        if (throwable != null) {
+            throw new RuntimeException("Error getting topic info", throwable);
+        }
+    }
+
+    private AdminClient getAdminClient() {
+        if (adminClient == null) {
+            final Properties props = new Properties();
+            props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+            props.put(ConsumerConfig.GROUP_ID_CONFIG, "group_1");
+            adminClient = AdminClient.create(props);
+        }
+        return adminClient;
+    }
+
+    public void deleteTopics(List<String> topic) {
+        getAdminClient().deleteTopics(topic);
+    }
+
+    private int getBoundPort(String listenerName) {
+        BrokerServer broker = cluster.brokers().values().iterator().next();
+        return broker.boundPort(ListenerName.normalised(listenerName));
+    }
 }
